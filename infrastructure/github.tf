28526bd128808e87794d31012f604375e79ae3c9
Francisco Cardoso <francardoso@outlook.com>
2023-02-17 16:28:22 -0300

    CleanUp: Remove pull_request trigger, add workflow_dipatch for facilitating future PRs

--- infrastructure/github.tf
+++ infrastructure/github.tf
@@ -8,10 +8,10 @@ resource "google_service_account" "github_actions" {
 resource "google_artifact_registry_repository" "main" {
   provider = google-beta
 
-  location = var.region
+  location      = var.region
   repository_id = var.service_name
-  description = "Application Docker repository"
-  format = "DOCKER"
+  description   = "Application Docker repository"
+  format        = "DOCKER"
 }
 
 resource "google_service_account_key" "github_actions" {
@@ -20,42 +20,46 @@ resource "google_service_account_key" "github_actions" {
 
 resource "google_cloud_run_service_iam_member" "github_actions_service_run_admin" {
   location = google_cloud_run_service.optic.location
-  service = google_cloud_run_service.optic.name
-  role = "roles/run.admin"// Cloud Run Admin
-  member = "serviceAccount:${google_service_account.github_actions.email}"
+  service  = google_cloud_run_service.optic.name
+  role     = "roles/run.admin" // Cloud Run Admin
+  member   = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
 resource "google_project_iam_binding" "github_actions_service_cloud_build" {
-  role = "roles/cloudbuild.builds.builder"// Cloud Build Service Account
+  project = var.gcp_project_id
+  role    = "roles/cloudbuild.builds.builder" // Cloud Build Service Account
   members = [
     "serviceAccount:${google_service_account.github_actions.email}"
   ]
 }
 
 resource "google_project_iam_binding" "github_actions_artifact_registry" {
-  role   = "roles/artifactregistry.writer" // Artifact Registry
+  project = var.gcp_project_id
+  role    = "roles/artifactregistry.writer" // Artifact Registry
   members = [
     "serviceAccount:${google_service_account.github_actions.email}"
   ]
 }
 
 resource "google_project_iam_member" "github_actions_service_storage_admin" {
+  project = var.gcp_project_id
   role    = "roles/storage.admin" // Storage Admin
-  member = "serviceAccount:${google_service_account.github_actions.email}"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
 resource "google_project_iam_member" "github_actions_service_account_user" {
+  project = var.gcp_project_id
   role    = "roles/iam.serviceAccountUser" // Service Account User
-  member = "serviceAccount:${google_service_account.github_actions.email}"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
 output "GCP_SA_EMAIL" {
   description = "GCP_SA_EMAIL GitHub Secret"
-  value = google_service_account.github_actions.email
+  value       = google_service_account.github_actions.email
 }
 
 output "GCP_SA_KEY" {
   description = "GCP_SA_KEY GitHub Secret"
-  value = google_service_account_key.github_actions.private_key
-  sensitive = true
+  value       = google_service_account_key.github_actions.private_key
+  sensitive   = true
 }
