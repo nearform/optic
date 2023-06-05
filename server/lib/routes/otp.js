c88e0e4f8e96ee72627454eb5ed009c1c53fda0a
Guilherme Kammsetzer <guilherme@kammsetzer.dev>
2023-06-05 18:37:19 -0300

    fix: move routes definition into loop to avoid repetition

--- server/lib/routes/otp.js
+++ server/lib/routes/otp.js
@@ -5,202 +5,123 @@ const uniqid = require('uniqid')
 const approvalLimit = 60e3
 
 async function otpRoutes(server) {
+  const otpRequestMethods = ['GET', 'POST']
   /**
    * @deprecated GET /api/generate/:token
    *
    * From now on the generate otp route will be a POST request
    * allowing the user to send more information about the OTP request such
    * as package name and version being deployed.
-   * The GET request is here just to maintain backwards compatibility
+   * The GET request is here just to maintain backwards compatibility and
+   * should be removed in the future.
    */
-  server.route({
-    method: 'GET',
-    url: '/api/generate/:token',
-    handler: async (request, reply) => {
-      const { firebaseAdmin, push } = server
-      const {
-        params: { token },
-        log
-      } = request
+  otpRequestMethods.forEach((method) => {
+    server.route({
+      method,
+      url: '/api/generate/:token',
+      handler: async (request, reply) => {
+        const { firebaseAdmin, push } = server
+        const {
+          params: { token },
+          log,
+          body = { packageInfo: {} }
+        } = request
 
-      const db = firebaseAdmin.firestore()
+        const db = firebaseAdmin.firestore()
 
-      const tokenData = await db.collection('tokens').doc(token).get()
+        const tokenData = await db.collection('tokens').doc(token).get()
 
-      if (!tokenData.exists) {
-        log.error('Token not found in tokens collection')
-        return reply.notFound('Token not found')
-      }
-
-      const { secretId, subscriptionId } = tokenData.data()
-
-      const subscription = await db
-        .collection('subscriptions')
-        .doc(subscriptionId)
-        .get()
-
-      if (!subscription.exists) {
-        log.error(`Subscription not found - ${subscriptionId}`)
-        return reply.notFound('Subscription not found')
-      }
-
-      const uniqueId = uniqid()
-
-      return new Promise(() => {
-        const requestObj = db.collection('requests').doc(uniqueId)
-        requestObj.set({ createdAt: new Date() })
-
-        const completeRequest = (error, otp) => {
-          requestObj.delete()
-          if (error) {
-            log.error(error.message)
-            return reply.internalServerError()
-          }
-
-          unsubscribe()
-          clearTimeout(timeout)
-
-          if (!otp) {
-            log.error('Request was not approved')
-            return reply.forbidden('Request was not approved')
-          }
-
-          log.info('Request approved, sending back OTP')
-          return reply.send(otp)
+        if (!tokenData.exists) {
+          log.error('Token not found in tokens collection')
+          return reply.notFound('Token not found')
         }
 
-        const unsubscribe = requestObj.onSnapshot(
-          (update) => {
-            const approved = update.get('approved')
-            if (approved === undefined) {
-              // update due to object creation
-              return
+        const { secretId, subscriptionId } = tokenData.data()
+
+        const subscription = await db
+          .collection('subscriptions')
+          .doc(subscriptionId)
+          .get()
+
+        if (!subscription.exists) {
+          log.error(`Subscription not found - ${subscriptionId}`)
+          return reply.notFound('Subscription not found')
+        }
+
+        const uniqueId = uniqid()
+
+        return new Promise(() => {
+          const requestObj = db.collection('requests').doc(uniqueId)
+          requestObj.set({ createdAt: new Date() })
+
+          const completeRequest = (error, otp) => {
+            requestObj.delete()
+            if (error) {
+              log.error(error.message)
+              return reply.internalServerError()
             }
-            completeRequest(null, update.get('otp'))
-          },
-          (error) => {
-            log.error(error.message)
-            return reply.internalServerError()
-          }
-        )
 
-        push.send({
-          subscription: subscription.data(),
-          secretId,
-          uniqueId,
-          token
-        })
+            unsubscribe()
+            clearTimeout(timeout)
 
-        const timeout = setTimeout(completeRequest, approvalLimit)
-      })
-    }
-  })
-
-  server.route({
-    method: 'POST',
-    url: '/api/generate/:token',
-    handler: async (request, reply) => {
-      const { firebaseAdmin, push } = server
-      const {
-        params: { token },
-        log,
-        body = { packageInfo: {} }
-      } = request
-
-      const db = firebaseAdmin.firestore()
-
-      const tokenData = await db.collection('tokens').doc(token).get()
-
-      if (!tokenData.exists) {
-        log.error('Token not found in tokens collection')
-        return reply.notFound('Token not found')
-      }
-
-      const { secretId, subscriptionId } = tokenData.data()
-
-      const subscription = await db
-        .collection('subscriptions')
-        .doc(subscriptionId)
-        .get()
-
-      if (!subscription.exists) {
-        log.error(`Subscription not found - ${subscriptionId}`)
-        return reply.notFound('Subscription not found')
-      }
-
-      const uniqueId = uniqid()
-
-      return new Promise(() => {
-        const requestObj = db.collection('requests').doc(uniqueId)
-        requestObj.set({ createdAt: new Date() })
-
-        const completeRequest = (error, otp) => {
-          requestObj.delete()
-          if (error) {
-            log.error(error.message)
-            return reply.internalServerError()
-          }
-
-          unsubscribe()
-          clearTimeout(timeout)
-
-          if (!otp) {
-            log.error('Request was not approved')
-            return reply.forbidden('Request was not approved')
-          }
-
-          log.info('Request approved, sending back OTP')
-          return reply.send(otp)
-        }
-
-        const unsubscribe = requestObj.onSnapshot(
-          (update) => {
-            const approved = update.get('approved')
-            if (approved === undefined) {
-              // update due to object creation
-              return
+            if (!otp) {
+              log.error('Request was not approved')
+              return reply.forbidden('Request was not approved')
             }
-            completeRequest(null, update.get('otp'))
-          },
-          (error) => {
-            log.error(error.message)
-            return reply.internalServerError()
+
+            log.info('Request approved, sending back OTP')
+            return reply.send(otp)
           }
-        )
 
-        const notification = {
-          subscription: subscription.data(),
-          secretId,
-          uniqueId,
-          token
-        }
-
-        if (body.packageInfo) {
-          notification.packageInfo = { ...body.packageInfo }
-        }
-
-        push.send(notification)
-
-        const timeout = setTimeout(completeRequest, approvalLimit)
-      })
-    },
-    schema: {
-      body: {
-        type: ['object', 'null'],
-        properties: {
-          packageInfo: {
-            type: 'object',
-            properties: {
-              name: { type: 'string' },
-              version: { type: 'string' }
+          const unsubscribe = requestObj.onSnapshot(
+            (update) => {
+              const approved = update.get('approved')
+              if (approved === undefined) {
+                // update due to object creation
+                return
+              }
+              completeRequest(null, update.get('otp'))
             },
-            required: ['name', 'version'],
-            additionalProperties: false
+            (error) => {
+              log.error(error.message)
+              return reply.internalServerError()
+            }
+          )
+
+          const notification = {
+            subscription: subscription.data(),
+            secretId,
+            uniqueId,
+            token
           }
-        },
-        additionalProperties: false
+
+          if (body.packageInfo) {
+            notification.packageInfo = { ...body.packageInfo }
+          }
+
+          push.send(notification)
+
+          const timeout = setTimeout(completeRequest, approvalLimit)
+        })
+      },
+      schema: method === 'POST' && {
+        body: {
+          type: ['object', 'null'],
+          properties: {
+            packageInfo: {
+              type: 'object',
+              properties: {
+                name: { type: 'string' },
+                version: { type: 'string' }
+              },
+              required: ['name', 'version'],
+              additionalProperties: false
+            }
+          },
+          additionalProperties: false
+        }
       }
-    }
+    })
   })
 
   server.post('/api/respond', async (request, reply) => {
