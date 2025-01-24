ba03a994077332cc6717e6cbc69ed87c664322fd
Ian Lovell <102812633+ianlnf@users.noreply.github.com>
2025-01-24 15:40:48 +0000

    feat: add otel instrumentation (#669)

---
+++ server/instrumentation.js
@@ -0,0 +1,63 @@
+const { NodeSDK } = require('@opentelemetry/sdk-node')
+const {
+  getNodeAutoInstrumentations
+} = require('@opentelemetry/auto-instrumentations-node')
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics')
+const {
+  OTLPTraceExporter
+} = require('@opentelemetry/exporter-trace-otlp-proto')
+const {
+  OTLPMetricExporter
+} = require('@opentelemetry/exporter-metrics-otlp-proto')
+const {
+  ATTR_SERVICE_NAME,
+  ATTR_SERVICE_VERSION
+} = require('@opentelemetry/semantic-conventions')
+const {
+  Resource,
+  envDetector,
+  processDetector,
+  hostDetector
+} = require('@opentelemetry/resources')
+
+const pkg = require('../package.json')
+
+require('dotenv').config()
+
+if (
+  !process.env.GRAFANA_OTLP_ENDPOINT ||
+  !process.env.GRAFANA_INSTANCE_ID ||
+  !process.env.GRAFANA_API_KEY
+) {
+  console.error('Grafana instrumentation not fully configured, skipping.')
+  process.exit(0)
+}
+
+const base64Key = Buffer.from(
+  `${process.env.GRAFANA_INSTANCE_ID}:${process.env.GRAFANA_API_KEY}`
+).toString('base64')
+
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [ATTR_SERVICE_NAME]: 'optic',
+    [ATTR_SERVICE_VERSION]: pkg.version
+  }),
+  resourceDetectors: [envDetector, processDetector, hostDetector],
+  traceExporter: new OTLPTraceExporter({
+    url: `${process.env.GRAFANA_OTLP_ENDPOINT}/v1/traces`,
+    headers: {
+      Authorization: `Basic ${base64Key}`
+    }
+  }),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+      url: `${process.env.GRAFANA_OTLP_ENDPOINT}/v1/metrics`,
+      headers: {
+        Authorization: `Basic ${base64Key}`
+      }
+    })
+  }),
+  instrumentations: [getNodeAutoInstrumentations()]
+})
+
+sdk.start()
